5f87f2acb0abd8bfd6fd8023aacfc92907bb8c87
Vladyslav Brusnyk <Brusnyk.V.Y@nmu.one>
2025-12-26 14:44:33 +0200

    refactor: use return statements for response methods in controllers

--- src/controllers/articlesController.js
+++ src/controllers/articlesController.js
@@ -1,15 +1,15 @@
 function listArticles(req, res) {
-    res.send('Articles list placeholder.');
+    return res.send('Articles list placeholder.');
 }
 
 function getArticle(req, res) {
     const { articleId } = req.params;
-    res.send(`Article details for ID: ${articleId}`);
+    return res.send(`Article details for ID: ${articleId}`);
 }
 
 function createArticle(req, res) {
     const { title } = req.body;
-    res.status(201).send(`Article created: ${title || 'Untitled'}`);
+    return res.status(201).send(`Article created: ${title || 'Untitled'}`);
 }
 
 module.exports = { listArticles, getArticle, createArticle };
